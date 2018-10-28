f568dc16cbbe0b6b4e4ecbdfdc7414eb0a0b6602
fitsumbeyene <beyeneaddis@yahoo.com>
2018-10-27 21:07:13 -0700

    the first assignment

---
+++ main.js
@@ -0,0 +1,66 @@
+'use strict'
+let students =prompt('Do youn think america was my dream land?')
+if(students.toLowerCase() === 'yes' ||
+    students.toUpperCase() === 'YES' ||
+      students.toLowerCase() === 'y' ||
+       students.toUpperCase() === 'Y')
+  {
+  alert(students + ', it is correct. amrica was one of my dream land.')
+    }
+    else{
+    alert(students + ', it is incorrect. america was not my dream land.')
+}
+  
+  let class201 =prompt('Do you think i am goint to finish this course?.')
+  if(class201.toLowerCase() === 'yes' ||
+     class201.toUpperCase() === 'YES' ||
+       class201.toLowerCase() === 'y' ||
+         class201.toUpperCase() === 'Y'){
+    alert(class201 + ', it is right, and i will finish it?.')
+  }else{
+      alert(class201 + ', it is incorrect, i am not you.')}
+
+     let student = prompt('is Fitsum the outstanding student in the class room?.')
+  if(student.toLowerCase() === 'yes' ||
+    student.toUpperCase() === 'YES' ||
+      student.toLowerCase() === 'y' ||
+         student.toUpperCase() === 'Y'){
+  alert(student + ',that is correct.')
+ }else { 
+   alert(student + ', is not the correct answer. fitsum is one of the outstanding student.')
+ }
+   let soccerbool = prompt('do you think abby is a fun of soccer')
+if(soccerbool.toLowerCase() === 'yes' ||
+    soccerbool.toUpperCase() === 'YES' ||
+      soccerbool.toLowerCase() === 'y' ||
+         soccerbool.toUpperCase() === 'Y'){
+    alert(soccerbool + ', it is wrong. and i am not the fun of soccerball.')
+}else{
+    alert(soccerbool + ', it is right. and i am not a fun of soccerball.')
+
+}
+let game = prompt('is javascript released in 1995?.')
+if(game.toLowerCase() === 'yes' ||
+    game.toUpperCase() === 'YES' ||
+      game.toLowerCase() === 'y' ||
+        game.toUpperCase() === 'Y' ){
+     alert(game + ', it is correct. javascript is released in 1995.')
+}else{
+     alert(game + ', it is incorrect. javasctipt is releasd in 1995.')
+    
+   }
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+
+   
+  
